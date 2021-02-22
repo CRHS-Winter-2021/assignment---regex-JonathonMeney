ef1c10948f6df4af232f7251bbf0781ea71ff558
Jonathon Meney <jome3395@cloud.edu.pe.ca>
2021-02-22 17:27:38 +0000

    dunzo

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 ### Assignment: Regex and Files
-##Name:
-##Date: 
+##Name: Jonathon Meney
+##Date: Feb. 22/2021
 
 #don't forget to import regex
-
+import re
 
 ##(/5) Task 1: MODIFY the code below.
 #A# Change the regex from .* to what is required to capture an email address
@@ -11,16 +11,19 @@
 #C# Count the number of email addresses found and print a final output line.
 
 def reEmail(fname):
-  
-  fhand = open(fname,'r')
-  for line in fhand:
-    extr = re.findall('.*',line)
-    if len(extr): 
-      print(extr)
-        
-
-  print()
+	fhand = open(fname,'r')
+	count = 0
+	for line in fhand:
+		line = line.rstrip()
+		extr = re.findall('\S*@\S*',line)
+		if len(extr):
+			count += 1
+			print(extr)
+	
+	print('There were ' + str(count) + ' email addresses in ' + fname)
     
+#reEmail('rural-staff.txt')
+
 
 '''### Task 1 Results for 
 >reEmail('rural-staff.txt')
@@ -35,11 +38,16 @@ There were 89 email addresses in rural-staff.txt
 #You should use a ( and ) regex like " (extract this) " to extract a portion of the match
 
 def reAward(fname):
-  fhand = open(fname, 'r')
-  for line in fhand:
-    extr = re.findall('.*', line)
-    if len(extr):
-      print(extr)
+	fhand = open(fname, 'r')
+	
+	for line in fhand:
+		line = line.rstrip()
+		extr = re.findall('- (.*)', line)
+		if len(extr):
+			print(extr)
+
+#reAward('rural-athletics.txt')
+
 
 '''### Task 2 Results for
 >reAward('rural-athletics.txt')
@@ -51,12 +59,22 @@ def reAward(fname):
 ##(/5) Task 3: CREATE code that will open a file and extract all the phone numbers 
 
 def rePhone(fname):
-  #open file
-  #loop through the file
-  #extract the specific phone numbers regex
-  #if the length of the extraction is not empty
-  #print the phone number
-  pass
+  
+	#open file
+	fhand = open(fname, 'r')
+
+	#loop through the file
+	for line in fhand:
+		line = line.rstrip()
+		#extract the specific phone numbers regex
+		extr = re.findall('902.*', line)
+		#if the length of the extraction is not empty
+		if len(extr):
+			#print the phone number
+			print(extr)
+
+#rePhone('rural-staff.txt')
+
 
 '''### Task 3 results
 >rePhone('rural-staff.txt')
